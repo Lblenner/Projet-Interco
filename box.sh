619b4e6c4340d916ab748173bccd9ecf26ca94ac
Louis <louis.blenner@gmail.com>
2019-12-11 10:58:30 +0100

    truc

--- box.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-sudo docker exec box bash -c "
-    ip addr flush dev eth0;
-    ip addr flush dev eth1;
-    ip addr flusg dev eth2;
-
-    ip addr add 192.168.0.1 dev eth2;
-    ip addr add 120.0.48.9/24 dev eth1"
