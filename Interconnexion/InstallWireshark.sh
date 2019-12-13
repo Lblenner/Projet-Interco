14e578915f8a7661b0c1b12e6f1edbace860ba42
root <louis.blenner@gmail.com>
2019-12-13 12:40:59 +0100

    Fix probleme -y apt install

--- Interconnexion/InstallWireshark.sh
+++ Interconnexion/InstallWireshark.sh
@@ -2,5 +2,8 @@
 
 sudo docker run -dit --name install --net=host --cap-add=NET_ADMIN debian;
 sudo docker exec install bash -c "apt update;
-                            apt install tshark;"
+                            apt install -y tshark;"
 sudo docker commit install debianwire
+sudo docker stop install
+sudo docker rm install
+
