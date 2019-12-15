ba00c8db06b716947cf5fe5493a218fb09f8d87a
mtardy <mahe5397@hotmail.fr>
2019-12-15 22:50:50 +0100

    Ne pas add une adresse sur eth0

--- reseau_entreprise/parefeu.sh
+++ reseau_entreprise/parefeu.sh
@@ -4,6 +4,5 @@ sudo docker exec parefeu bash -c "ip addr flush dev eth1;
     ip addr flush dev eth0;
     ip addr flush dev eth2;
 
-    ip addr add 120.0.48.2/24 dev eth0;
     ip addr add 120.0.48.3/24 dev eth1;
     ip addr add 120.0.48.9/24 dev eth2"
