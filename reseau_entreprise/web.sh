c694c29f7a153a227402c0058ef22a909d352529
mtardy <mahe5397@hotmail.fr>
2019-12-16 21:17:59 +0100

    Ajout firewall sur serveur web

--- reseau_entreprise/web.sh
+++ reseau_entreprise/web.sh
@@ -4,6 +4,23 @@ sudo docker exec web bash -c "apt-get update;
     apt-get install -y nginx;
     ip addr flush dev eth1;
     ip addr flush dev eth0;
-
     service nginx start;
+    
+    # Firewall
+        iptables -t filter -P INPUT DROP;
+        iptables -t filter -P FORWARD DROP;
+        iptables -t filter -P OUTPUT DROP;
+        # Autoriser loopback
+        iptables -t filter -A INPUT -i lo -j ACCEPT;
+        iptables -t filter -A OUTPUT -o lo -j ACCEPT;
+        # Autoriser icmp
+        iptables -t filter -A INPUT -p icmp -j ACCEPT;
+        iptables -t filter -A OUTPUT -p icmp -j ACCEPT;
+        # Autoriser connexion établies
+        iptables -A INPUT -m state --state ESTABLISHED -j ACCEPT;
+        iptables -A OUTPUT -m state --state ESTABLISHED -j ACCEPT;
+        # Autoriser tcp sur le port 80
+        iptables -t filter -A INPUT -p tcp --dport 80 -j ACCEPT;
+        iptables -t filter -A OUTPUT -p tcp --dport 80 -j ACCEPT;
+
     ip addr add 120.0.48.4/24 dev eth1"
